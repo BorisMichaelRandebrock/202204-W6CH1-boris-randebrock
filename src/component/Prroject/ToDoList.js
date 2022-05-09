2b6ea88cc1eca0edf308018b661330d35bc32419
Boris <boris@randebrock.com>
2022-05-09 18:46:10 +0200

    favicon changed as it did not appear

---
+++ src/component/Prroject/ToDoList.js
@@ -0,0 +1,12 @@
+import toDoArray from "../../toDoArray";
+
+const ToDoList = () => {
+  return (
+    <ul>
+      <li>`${toDoArray}`</li>
+      <li></li>
+      <li></li>
+    </ul>
+  );
+};
+export default ToDoList;
---
+++ src/toDoArray.js
@@ -0,0 +1,8 @@
+const toDoArray = [
+  {
+    id: 1,
+    name: "string",
+    done: false,
+  },
+];
+export default toDoArray;
