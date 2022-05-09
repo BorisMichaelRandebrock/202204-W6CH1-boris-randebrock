ee1049c97f3a1f721cf5573fabae08d484df2b9a
Boris <boris@randebrock.com>
2022-05-09 20:48:22 +0200

    lots of small changes all over

--- src/component/ToDoList/ToDoList.js
+++ src/component/ToDoList/ToDoList.js
@@ -1,11 +1,17 @@
+import { useSelector } from "react-redux";
+
+import ToDo from "../toDo/ToDo";
+
 const ToDoList = () => {
+  const todo = useSelector((state) => state.toDo);
+
   return (
     <>
       <h1>To Do List</h1>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+        {todo.map((todo) => (
+          <ToDo todo={todo} />
+        ))}
       </ul>
     </>
   );
