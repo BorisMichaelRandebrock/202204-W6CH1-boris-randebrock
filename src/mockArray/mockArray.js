78dd5c2bf474a814ef1f0c79c62f5f7d86606d05
Boris <boris@randebrock.com>
2022-05-10 14:52:48 +0200

    silly tests, wrongly executed, to get the hang on it

---
+++ src/mockArray/mockArray.js
@@ -0,0 +1,13 @@
+const mockArray = [
+  {
+    id: 1,
+    name: "Wine",
+    done: false,
+  },
+  {
+    id: 2,
+    name: "Beer",
+    done: false,
+  },
+];
+export default mockArray;
--- src/redux/feature/toDoSlice.test.js
+++ src/redux/feature/toDoSlice.test.js
@@ -1,5 +1,50 @@
+import mockArray from "../../mockArray/mockArray";
+import toDoSlice, {
+  addToDoSActionCreator,
+  removeToDosActionCreator,
+} from "./toDoSlice";
+
 describe("Given the toDoSLice component", () => {
-  describe("When rendered with a shopping list of 1 Wine item", () => {
-    test("Then it should always match this snapshot", () => {});
+  describe("When rendered with the action type empty", () => {
+    test("Then it should return an array with two items", () => {
+      const givenAction = {
+        type: "",
+        payload: addToDoSActionCreator,
+      };
+
+      const array = mockArray;
+      const arrayLength = 2;
+      const expectedResult = toDoSlice(array, givenAction);
+
+      expect(expectedResult).toHaveLength(arrayLength);
+    });
+  });
+  describe("When rendered with the action 'addToDoSActionCreator'", () => {
+    test("Then it should render an array with 3 list items", () => {
+      const givenAction = {
+        type: addToDoSActionCreator,
+        payload: addToDoSActionCreator,
+      };
+
+      const array = mockArray;
+      const arrayLength = 3;
+      const expectedResult = toDoSlice(array, givenAction);
+
+      expect(expectedResult).toHaveLength(arrayLength);
+    });
+  });
+  describe("When rendered with the action 'removeToDosActionCreator'", () => {
+    test("Then Iit should render an array with 1 listItem", () => {
+      const givenAction = {
+        type: removeToDosActionCreator,
+        payload: addToDoSActionCreator,
+      };
+
+      const array = mockArray;
+      const arrayLength = 1;
+      const expectedResult = toDoSlice(array, givenAction);
+
+      expect(expectedResult).toHaveLength(arrayLength);
+    });
   });
 });
